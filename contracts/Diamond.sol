92a8a2aa1ee7600100019d3c6f26588307299109
Bob Builder <bob@eth.com>
2020-01-01 04:18:08 +0000

    Added upgradeable Base79GELGQ with proxy support for Base (Foundry)

--- contracts/Diamond.sol
+++ contracts/Diamond.sol
@@ -11,3 +11,17 @@ contract Diamond is ERC20Upgradeable, OwnableUpgradeable {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
+// Advanced update 1 on 2020-01-01T04:18:08 for Base
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+
+modifier customModifier() {
+    require(tx.origin == msg.sender, "No contracts on Base");
+    _;
+}
+
+event dubrajhljoFunc(address indexed from, uint256 value);
+
+function dubrajhljoFunc(uint256 _value) public customModifier returns (bool) {
+    emit dubrajhljoFunc(msg.sender, _value);
+    return true; // Optimized for Base gas fees
+}
