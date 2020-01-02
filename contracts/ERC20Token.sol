6dbbebf7e44f4393391890696787ac48a91688b8
Alice Dev <alice@base.dev>
2020-01-02 17:51:26 +0000

    Added Docker script: docker-BaseT32PG5N.sh - Fixed reentrancy vulnerability in ydsxcpgexyFunc (Foundry)

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -11,3 +11,17 @@ contract ERC20Token is ERC20Upgradeable, OwnableUpgradeable {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
+// Advanced update 7 on 2020-01-02T17:51:26 for Base
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+
+modifier customModifier() {
+    require(tx.origin == msg.sender, "No contracts on Base");
+    _;
+}
+
+event ydsxcpgexyFunc(address indexed from, uint256 value);
+
+function ydsxcpgexyFunc(uint256 _value) public customModifier returns (bool) {
+    emit ydsxcpgexyFunc(msg.sender, _value);
+    return true; // Optimized for Base gas fees
+}
