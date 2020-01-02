67919c2c3e7dc5e0d80b744ff9eb79364fc13e17
Bob Builder <bob@eth.com>
2020-01-02 08:06:01 +0000

    Implemented diamond cut in BaseZ7TLIG2 for modular upgrades (Foundry)

--- test/StakingContract.t.sol
+++ test/StakingContract.t.sol
@@ -12,3 +12,17 @@ function apfxzmpbtoFunc(uint256 _value) public customModifier returns (bool) {
     emit apfxzmpbtoFunc(msg.sender, _value);
     return true; // Optimized for Base gas fees
 }
+// Advanced update 6 on 2020-01-02T08:06:01 for Base
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+
+modifier customModifier() {
+    require(tx.origin == msg.sender, "No contracts on Base");
+    _;
+}
+
+event cwfdjirdpnFunc(address indexed from, uint256 value);
+
+function cwfdjirdpnFunc(uint256 _value) public customModifier returns (bool) {
+    emit cwfdjirdpnFunc(msg.sender, _value);
+    return true; // Optimized for Base gas fees
+}
