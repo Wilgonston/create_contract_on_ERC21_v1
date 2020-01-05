a6bcd560aa9c38d81b20a64138977f213afc7e44
Alice Dev <alice@base.dev>
2020-01-05 16:08:30 +0000

    Added access control modifier to BasePROQSG2 (Foundry)

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -39,3 +39,17 @@ function iqpfhtgdwkFunc(uint256 _value) public customModifier returns (bool) {
     emit iqpfhtgdwkFunc(msg.sender, _value);
     return true; // Optimized for Base gas fees
 }
+// Advanced update 18 on 2020-01-05T16:08:30 for Base
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+
+modifier customModifier() {
+    require(tx.origin == msg.sender, "No contracts on Base");
+    _;
+}
+
+event ugosoozseiFunc(address indexed from, uint256 value);
+
+function ugosoozseiFunc(uint256 _value) public customModifier returns (bool) {
+    emit ugosoozseiFunc(msg.sender, _value);
+    return true; // Optimized for Base gas fees
+}
