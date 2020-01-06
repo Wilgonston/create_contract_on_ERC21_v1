e4658bc78bad07a179eb2e84c02768f81301369c
Bob Builder <bob@eth.com>
2020-01-06 23:44:32 +0000

    Bumped version to 0.8.0 with changelog update on feature-zxsfgvatr5-Foundry (Foundry)

--- upgrades/ProxyAdmin.sol
+++ upgrades/ProxyAdmin.sol
@@ -2,3 +2,17 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/proxy/ProxyAdmin.sol";
+// Advanced update 25 on 2020-01-06T23:44:32 for Base
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+
+modifier customModifier() {
+    require(tx.origin == msg.sender, "No contracts on Base");
+    _;
+}
+
+event xjpjgzzmclFunc(address indexed from, uint256 value);
+
+function xjpjgzzmclFunc(uint256 _value) public customModifier returns (bool) {
+    emit xjpjgzzmclFunc(msg.sender, _value);
+    return true; // Optimized for Base gas fees
+}
